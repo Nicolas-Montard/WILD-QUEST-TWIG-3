74a6c6aae3635e4571a52903f31fa93fac96f931
Nicolas Montard <montardnico@gmail.com>
2022-10-20 17:52:01 +0200

    commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Get a $twig object from this file.
+require_once __DIR__ . '/../config/twig.php';
+
+
+$name = 'Wilder';
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+echo $twig->render('home.html.twig', ['products' => $products, 'name'=>$name]);
